c82aa8e97c1a935fb045e89dfb245d5c755eb48c
Andrew <andrewallison5211@gmail.com>
2023-06-12 15:45:24 -0600

    JavaIntro

---
+++ app.js
@@ -0,0 +1,33 @@
+// console.log("It works");
+
+let initialHomeScore = 0
+
+let initialAwayScore = 0
+
+
+
+let userInput = ""
+
+
+
+function increaseHomeScore() {
+  initialHomeScore++
+  console.log('This is the home score:', initialHomeScore);
+
+  draw()
+}
+
+function increaseAwayScore() {
+  initialAwayScore++
+  console.log('This is the away score:', initialAwayScore);
+  draw()
+}
+
+function draw() {
+  // @ts-ignore
+  document.getElementById('homeStartScore').innerText = initialHomeScore.toString()
+  // @ts-ignore
+  document.getElementById('awayStartScore').innerText = initialAwayScore
+}
+
+draw()
